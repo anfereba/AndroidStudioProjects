c94d1b77843177b30f2893fc6a313a9e3ae3abfe
anfereba <49685089+anfereba@users.noreply.github.com>
2021-09-23 20:04:17 -0500

    Listando Imagenes Categoria Serie

--- FondosLollipop/app/src/main/java/com/anfereba/Fondosdepantalla/CategoriasAdmin/MusicaA/MusicaA.java
+++ FondosLollipop/app/src/main/java/com/anfereba/Fondosdepantalla/CategoriasAdmin/MusicaA/MusicaA.java
@@ -25,7 +25,6 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class MusicaA extends AppCompatActivity {
 
-
     RecyclerView recyclerViewMusica;
     FirebaseDatabase mFirebaseDataBase;
     DatabaseReference mRef;
--- FondosLollipop/app/src/main/java/com/anfereba/Fondosdepantalla/CategoriasAdmin/SeriesA/SeriesA.java
+++ FondosLollipop/app/src/main/java/com/anfereba/Fondosdepantalla/CategoriasAdmin/SeriesA/SeriesA.java
@@ -3,17 +3,33 @@ package com.anfereba.Fondosdepantalla.CategoriasAdmin.SeriesA;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.anfereba.Fondosdepantalla.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SeriesA extends AppCompatActivity {
+
+    RecyclerView recyclerViewSerie;
+    FirebaseDatabase mFirebaseDataBase;
+    DatabaseReference mRef;
+
+    FirebaseRecyclerAdapter<Serie, ViewHolderSerie> firebaseRecyclerAdapter;
+    FirebaseRecyclerOptions<Serie> options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +41,65 @@ public class SeriesA extends AppCompatActivity {
         actionBar.setTitle("Series");
         actionBar.setDisplayShowHomeEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        recyclerViewSerie = findViewById(R.id.reciclerViewSerie);
+        recyclerViewSerie.setHasFixedSize(true);
+
+        mFirebaseDataBase = FirebaseDatabase.getInstance();
+        mRef = mFirebaseDataBase.getReference("Series");
+
+        ListarImagenesSerie();
+
     }
 
+    private void ListarImagenesSerie() {
+        options = new FirebaseRecyclerOptions.Builder<Serie>().setQuery(mRef,Serie.class).build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Serie, ViewHolderSerie>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ViewHolderSerie viewHolderSerie, int i, @NonNull Serie serie) {
+                viewHolderSerie.SeteoSeries(
+                        getApplicationContext(),
+                        serie.getNombre(),
+                        serie.getVistas(),
+                        serie.getImagen()
+                );
+            }
+
+            @NonNull
+            @Override
+            public ViewHolderSerie onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                //Inflar el Item
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_serie,parent, false);
+                ViewHolderSerie viewHolderSerie = new ViewHolderSerie(itemView);
+
+                viewHolderSerie.setOnClickListener(new ViewHolderSerie.ClickListener() {
+                    @Override
+                    public void onItemClick(View view, int posicion) {
+                        Toast.makeText(SeriesA.this, "ITEM CLICK", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onItemLongClick(View view, int position) {
+                        Toast.makeText(SeriesA.this, "LONG CLICK", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                return viewHolderSerie;
+            }
+        };
+        recyclerViewSerie.setLayoutManager(new GridLayoutManager(SeriesA.this,2));
+        firebaseRecyclerAdapter.startListening();
+        recyclerViewSerie.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (firebaseRecyclerAdapter!=null){
+            firebaseRecyclerAdapter.startListening();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();

702ef8fd5b4e9b9de940987d7fc98eaf3a016f37
anfereba <49685089+anfereba@users.noreply.github.com>
2021-09-27 07:58:39 -0500

    Leyendo informacion del administrador de la base de datos

--- FondosLollipop/app/src/main/java/com/anfereba/Fondosdepantalla/FragmentosAdministrador/PerfilAdmin.java
+++ FondosLollipop/app/src/main/java/com/anfereba/Fondosdepantalla/FragmentosAdministrador/PerfilAdmin.java
@@ -2,65 +2,101 @@ package com.anfereba.Fondosdepantalla.FragmentosAdministrador;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.anfereba.Fondosdepantalla.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PerfilAdmin#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PerfilAdmin extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //Firebase
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    DatabaseReference BASE_DE_DATOS_ADMINISTRADORES;
 
-    public PerfilAdmin() {
-        // Required empty public constructor
-    }
+    //Vistas
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PerfilAdmin.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PerfilAdmin newInstance(String param1, String param2) {
-        PerfilAdmin fragment = new PerfilAdmin();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    ImageView FOTOPERFILIMG;
+    TextView UIDPERFIL, NOMBRESPERFIL,APELLIDOSPEFIL,CORREOPERFIL,PASSWORDPERFIL,EDADPERFIL;
+    Button ACTUALIZARPASS,ACTUALIZARDATOS;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil_admin, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil_admin, container, false);
+
+        FOTOPERFILIMG = view.findViewById(R.id.FOTOPERFILIMG);
+        UIDPERFIL = view.findViewById(R.id.UIDPERFIL);
+        NOMBRESPERFIL = view.findViewById(R.id.NOMBRESPERFIL);
+        APELLIDOSPEFIL = view.findViewById(R.id.APELLIDOSPERFIL);
+        CORREOPERFIL = view.findViewById(R.id.CORREOPERFIL);
+        PASSWORDPERFIL = view.findViewById(R.id.PASSWORDPERFIL);
+        EDADPERFIL = view.findViewById(R.id.EDADPERFIL);
+        ACTUALIZARPASS = view.findViewById(R.id.ACTUALIZARPASS);
+        ACTUALIZARDATOS = view.findViewById(R.id.ACTUALIZARDATOS);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        BASE_DE_DATOS_ADMINISTRADORES = FirebaseDatabase.getInstance().getReference("BASE DE DATOS ADMINISTRADORES");
+        BASE_DE_DATOS_ADMINISTRADORES.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    //OBTENER LOS DATOS
+                    String uid = ""+snapshot.child("UID").getValue();
+                    String nombre = ""+snapshot.child("NOMBRES").getValue();
+                    String apellidos = ""+snapshot.child("APELLIDOS").getValue();
+                    String correo = ""+snapshot.child("CORREO").getValue();
+                    String pass = ""+snapshot.child("PASSWORD").getValue();
+                    String edad = ""+snapshot.child("EDAD").getValue();
+                    String imagen = ""+snapshot.child("IMAGEN").getValue();
+
+                    UIDPERFIL.setText(uid);
+                    NOMBRESPERFIL.setText(nombre);
+                    APELLIDOSPEFIL.setText(apellidos);
+                    CORREOPERFIL.setText(correo);
+                    PASSWORDPERFIL.setText(pass);
+                    EDADPERFIL.setText(edad);
+
+                    try{
+                        //Si existe la iamgen, cargue el string y luego la imagen
+                        Picasso.get().load(imagen).placeholder(R.drawable.perfil).into(FOTOPERFILIMG);
+                    }catch ( Exception e){
+                        //No existe imagen en la bd del administrador
+                        Picasso.get().load(R.drawable.perfil).into(FOTOPERFILIMG);
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+        return view;
     }
 }
